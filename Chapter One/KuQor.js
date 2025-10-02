056ca2933b0c2cbb12e72e2b641bd1588ef01565
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-10-02 13:34:31 +0300

    Delete Chapter One directory

--- Chapter One/KuQor.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-fs.writeFile("./file2.txt", "Xogtaan waxaan Rabaa in ay Ka soo Muuqato File1", (error) => {
-    if (error) throw error;
-    console.log("Xogtaan waa la save gareye");
-});
